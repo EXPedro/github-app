60ff82fbcf250190a5516bd05dc73a2318c75e25
EXPedro <pedroelisio555@gmail.com>
2021-09-07 14:19:24 -0300

    Update profile

--- src/components/profile/index.js
+++ src/components/profile/index.js
@@ -3,8 +3,10 @@ import * as S from './styled'
 
 function Profile(){
     return <S.Wrapper>
-
-
+        <S.WrapperImage />
+        <S.WrapperInfoUser />
+        <S.WrapperUserName />
+        <S.WrapperStatusCount />
     </S.Wrapper>
 }
 
