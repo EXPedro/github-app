d15d86a2f7753612cb85a646b844373b8ba53c06
EXPedro <pedroelisio555@gmail.com>
2021-09-07 21:12:10 -0300

    Create mock user

--- src/components/profile/index.js
+++ src/components/profile/index.js
@@ -3,10 +3,36 @@ import * as S from './styled'
 
 function Profile(){
     return <S.Wrapper>
-        <S.WrapperImage />
-        <S.WrapperInfoUser />
-        <S.WrapperUserName />
-        <S.WrapperStatusCount />
+            <S.WrapperImage 
+                src="https://avatars.githubusercontent.com/u/80805367?v=4" 
+                alt="Avatar of user" />
+            <S.WrapperInfoUser>
+                <div>
+                    <h1>Elisio Xavier Pedro</h1>
+                    <S.WrapperUserName>
+                        <h3>Username: </h3>
+                        <a href="https://github.com/EXPedro" 
+                           target="_blank" 
+                           rel="noreferrer">
+                               EXPedro
+                        </a>
+                    </S.WrapperUserName>
+                </div>    
+                <S.WrapperStatusCount>
+                    <div>
+                        <h4>Followers</h4>
+                        <span>5</span>
+                    </div>
+                    <div>                        
+                        <h4>Starreds</h4>
+                        <span>5</span>
+                    </div>
+                    <div>                        
+                        <h4>Followings</h4>
+                        <span>5</span>
+                    </div>
+                </S.WrapperStatusCount>
+            </S.WrapperInfoUser>    
     </S.Wrapper>
 }
 
