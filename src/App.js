227f8e1cf93981b0c3b9d3d8f8d84c030a2bb98e
EXPedro <pedroelisio555@gmail.com>
2021-09-08 12:32:09 -0300

    Update repositories styles

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Layout from './components/layout'
 import Profile from './components/profile';
+import Repositories from './components/repositories';
 import { ResetCSS } from './global/resetCSS';
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
      <ResetCSS />
      <Layout>
        <Profile />
+       <Repositories />
      </Layout>
    </main>
 }
