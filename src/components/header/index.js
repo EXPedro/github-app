b3c58c5f8d82dc89c5a5dac714aee061b2003ddd
EXPedro <pedroelisio555@gmail.com>
2021-09-08 23:24:59 -0300

    Update header component, hooks and providers

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,16 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 import * as S from './styled'
+import useGithub from '../../hooks/github-hooks'
 
 const Header = () => {
-    return <header>
-        <S.Wrapper>
-            <input 
-                type="text"
-                placeholder="Digite o username para pesquisa..." />
-            <button>Buscar</button>
-        </S.Wrapper>
-    </header>
+    const {getUser} = useGithub();
+    const [userNameForSearch, getUserNameForSearch] = useState();
 
+    const submitGetUser = () =>{
+        if(!userNameForSearch) return;
+        return getUser(userNameForSearch);
+    }
+    return( 
+        <header>
+            <S.Wrapper>
+                <input 
+                    type="text"
+                    placeholder="Digite o username para pesquisa..." 
+                    onChange={submitGetUser}
+                />
+                <button>Buscar</button>
+            </S.Wrapper>
+        </header>
+    )
 }
 
 export default Header;
