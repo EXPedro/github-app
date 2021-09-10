6bd31a8fcbda046e6a38d8156c79228588ddfcca
EXPedro <pedroelisio555@gmail.com>
2021-09-10 11:52:47 -0300

    Update profile, repositories, repository-item

--- src/components/repositories/styled.js
+++ src/components/repositories/styled.js
@@ -1,53 +1,54 @@
 import styled from "styled-components";
-import { Tabs, TabList, Tab, TabPanel } from 'react-tabs';
+import { Tabs, TabList, Tab, TabPanel } from "react-tabs";
 
 export const WrapperTabs = styled(Tabs)`
-    font-size: 16px;
-    width: 100%;
-    margin-top: 16px;
+  font-size: 16px;
+  width: 100%;
+  margin-top: 16px;
 `;
 
 export const WrapperTabList = styled(TabList)`
-    list-style-type: none;
-    padding: 4px;
-    display: flex;
-    margin: 0;
+  list-style-type: none;
+  padding: 4px;
+  display: flex;
+  margin: 0;
 `;
-
-WrapperTabList.tabsRole = 'TabList';
+WrapperTabList.tabsRole = "TabList";
 
 export const WrapperTab = styled(Tab)`
-    border-radius: 16px;
-    border: 1px solid #ccc;
-    padding: 16px;
-    user-select: none;
-    cursor: pointer;
-    z-index: 99999;
-    background-color: '#fff';
-    margin: 8px;
+  border-radius: 16px;
+  border: 1px solid #ccc;
+  padding: 16px;
+  user-select: none;
+  cursor: pointer;
+  z-index: 99999;
+  background-color: "#fff";
+  margin: 8px;
 
-    &:focus{
-        outline: none;
-    }
+  &:focus {
+    outline: none;
+  }
 
-    &.is-selected{
-        box-shadow: 3px 2px 10px rgba(0, 0, 0, 0.2);
-    }
+  &.is-selected {
+    box-shadow: 3px 2px 10px rgba(0, 0, 0, 0.2);
+  }
 `;
-
-WrapperTab.tabsRole = 'Tab';
+WrapperTab.tabsRole = "Tab";
 
 export const WrapperTabPanel = styled(TabPanel)`
-    display: none;
-    min-height: 40vh;
-    margin-top: -5px;
+  padding: 16px;
+  border: 1px solid "#ccc";
+  display: none;
+  margin-top: -5px;
 
-    padding: 16px;
-    border: 1px solid "#ccc";
-
-    &.is-selected{
-        display: block;
-    }
+  &.is-selected {
+    display: block;
+  }
 `;
+WrapperTabPanel.tabsRole = "TabPanel";
 
-WrapperTabPanel.tabsRole = 'TabPanel';
+export const WrapperList = styled.div`
+  display: flex;
+  justify-content: flex-start;
+  flex-wrap: wrap;
+`;
